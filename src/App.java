59b98a804ba6b56c9abd30b5bbafe9407d7ee6c6
FernandMejia19 <fercholkm19@gmail.com>
2024-04-10 17:53:57 -0500

    ADD: Metodo BubbleSort

--- src/App.java
+++ src/App.java
@@ -1,8 +1,14 @@
+///import java.util.Scanner;
 public class App {
+    
     public static void main(String[] args) throws Exception {
         int []numeros={ 30, 7, 12 , 5};
         MetodosOredenamiento metodosOrdenamiento=new MetodosOredenamiento() ;
-        metodosOrdenamiento.sorbyBubble(numeros);
+        /*String preg;
+        Scanner leer=new Scanner (System.in);
+        preg=leer.nextLine();*/
+        metodosOrdenamiento.imprimir(numeros);
+        metodosOrdenamiento.sorbyBubble(numeros,false);
         metodosOrdenamiento.imprimir(numeros);
     }
 }
--- src/MetodosOredenamiento.java
+++ src/MetodosOredenamiento.java
@@ -1,23 +1,49 @@
+import java.util.Scanner;
 public class MetodosOredenamiento {
+    
     ///metodo que devuelve un arreglo de enteros
     ///ordenado por burbuja
-    public int [] sorbyBubble (int[]arreglo){
+    public int[] sorbyBubble (int[]arreglo,boolean logs){
 
         ///obtener el tamano del arreglo
+        System.out.println("SI DESEA IMPRIMIR EL PROECESO DIGITE 1\nSI NO DESEA IMRIMIR EL RESULTADO DIGITE 0 ");
+        Scanner leer=new Scanner (System.in);
+        int preg;
+        preg=leer.nextInt();
+        if (preg==1){
+            logs=true;
+        }
+        else if (preg==0){
+            logs=false;
+
+        }
         int tamano=arreglo.length;
 
         ///bucle externo que va a recorrer todo el arreglo
 
         for (int i=0;i<tamano;i++){
             //Bucle for interno que compara el elemento actual con el siguiente
+            if (logs){
+                System.out.println("\n Pasada numero "+i);
+            }
             for (int j=i+1;j<tamano;j++){
+                if (logs){
+                    System.out.println("i=  "+arreglo[i]+"  j= "+arreglo[j]);
+                }
                 int aux=0;
 
-                if (arreglo[i]<arreglo[j]){
+                if (arreglo[i]>arreglo[j]){
+                    if (logs){
+                        System.out.println("Si es mayor asi que cambio ");
+                    }
 
                     aux=arreglo[i];
                     arreglo[i]=arreglo[j];
                     arreglo[j]=aux;
+                    if (logs){
+                        System.out.println(" ");
+                        imprimir(arreglo);
+                    }
 
                 }
 
@@ -25,11 +51,20 @@ public class MetodosOredenamiento {
             
         }
         return arreglo;
+        
     }
     public void imprimir(int[] arreglo){
         for (int item:arreglo){
             System.out.print(item + " ");
 
         }
+        System.out.println(" ");
     }
+    /* 
+    public void Preguntar(String []preg){
+        Scanner leer=new Scanner (System.in);
+        preg=leer.nextLine();
+
+
+    }*/
 }
